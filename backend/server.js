d8528f8c8bc08f7a9da324c0bc068944a55fe431
DanielRed007 <danielrojo1927@gmail.com>
2021-05-16 12:28:16 -0300

    course state: integration between FE and BE, concurrently working with npm scripts

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Api OK!!");
+});
+
+app.get("/api/products", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.json(product);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(
+  port,
+  console.log(`Server running in ${process.env.NODE_ENV} on port: ${port}`)
+);
---
+++ client/src/components/Product.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 bg-light rounded">
+      <Link href={`/product/${product._id}`}>
+        <Card.Img src={product.image}></Card.Img>
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title className="text-dark">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text className="text-dark" as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+
+        <Card.Text className="text-dark" as="h3">
+          {product.price}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
