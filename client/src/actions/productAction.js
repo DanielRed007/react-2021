eeffd6f63b336b8391a2e12f4a8858bf4aba12f7
DanielRed007 <danielrojo1927@gmail.com>
2021-05-27 21:19:21 -0300

    product details reducers and state and constants for reducer actions were added

--- client/src/actions/productAction.js
+++ client/src/actions/productAction.js
@@ -1,20 +1,45 @@
 import axios from "axios";
-import { PRODUCT_LIST_FAILED, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_REQUEST } from "../constants/productConstants";
+import {
+  PRODUCT_LIST_FAILED,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAILED,
+} from "../constants/productConstants";
 
-export const listProducts = () => async(dispatch) => {
-    try {
-        dispatch({type: PRODUCT_LIST_REQUEST});
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
 
-        const { data } = await axios.get("/api/products");
+    const { data } = await axios.get("/api/products");
 
-        dispatch({
-            type: PRODUCT_LIST_SUCCESS,
-            payload: data
-        });
-    } catch (error) {
-        dispatch({
-            type: PRODUCT_LIST_FAILED,
-            payload: error
-        });
-    }
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAILED,
+      payload: error,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAILED,
+      payload: error,
+    });
+  }
 };
